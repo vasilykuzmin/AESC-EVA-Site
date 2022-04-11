d376eb09547bb79b8ee23f8a6128b26ace7a27e1
Vasiliy Kuzmin <vasilykuzmin22@gmail.com>
2022-04-11 13:29:58 +0300

    Initial commit. Simple index page.

---
+++ AESC_EVA_Site/AESC_EVA_Site/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AESC_EVA_Site project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AESC_EVA_Site.settings')
+
+application = get_asgi_application()
