47e5649ddfdbe04b4a58480b8e211c149d1410a2
Ziware <andry.polikarpov@mail.ru>
2022-04-27 10:07:55 +0300

    Добавил сочненького

--- AESC_EVA_Site/mainapp/urls.py
+++ AESC_EVA_Site/mainapp/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
+    path('wiki', views.wiki, name='wiki'),
     path('posters', views.posters, name='posters'),
+    path('rock', views.rock, name='rock'),
+    path('authors', views.authors, name='authors'),
     path('rickroll', views.rickroll, name='rickroll'),
 
 ]
--- AESC_EVA_Site/mainapp/views.py
+++ AESC_EVA_Site/mainapp/views.py
@@ -12,3 +12,12 @@ def rickroll(request):
 
 def posters(request):
     return render(request, 'mainapp/posters.html', context={})
+
+def wiki(request):
+    return render(request, 'mainapp/wiki.html', context={})
+
+def rock(request):
+    return render(request, 'mainapp/rock.html', context={})
+
+def authors(request):
+    return render(request, 'mainapp/authors.html', context={})
